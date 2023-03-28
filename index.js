899a1d73d5b783a79ba31f38f453e2a4dad420da
dipjoy <dipjoy488@gmail.com>
2023-03-29 02:27:15 +0600

    a

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const webPush = require('web-push')
+require('dotenv').config()
 const cors = require('cors')
 const path = require('path')
 const { MongoClient } = require("mongodb");
@@ -9,9 +10,12 @@ app.use(express.json())
 
 app.use(cors())
 
+const url= process.env.url
+console.log(url)
+
 
 // const url = "mongodb+srv://djd:xFyJBEhWmMyL1Aau@cluster0.5ftqt.mongodb.net/?retryWrites=true&w=majority"
-const urI ="mongodb+srv://vercel-admin-user:U0kAQwgs85V7fFr7@cluster0.5ftqt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+// const urI ="mongodb+srv://vercel-admin-user:U0kAQwgs85V7fFr7@cluster0.5ftqt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const client = new MongoClient(url);
 // const publicvidkey ="BKoQKT0Fgo5NBXCQu-YAeCWZAw8dj-HcJnqDyS5qkYBZYGAhrZ7JByt1eFQzdqaCMsQzL95beJfgOiyTK_TRlgg "
 // const privatevidkey = "bWlzmatH7YoKfPAzba6NqBAKyFLuDx0OE9tvId4kQk4"
